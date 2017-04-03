f240daa24aa702c568925ef6ace426271186e153
Nat Hillard <nat.hillard@willowtreeapps.com>
2017-04-03 00:15:58 -0400

    proper retry handling

--- GoogleSignInAPITV/GoogleSignInController.swift
+++ GoogleSignInAPITV/GoogleSignInController.swift
@@ -9,80 +9,81 @@
 import Foundation
 import PromiseKit
 
-struct GoogleSignInController {
-    func requestPresentationCode() -> Promise<(String, String, Int)> {
-        return Promise { fulfill, reject in
-            var components = URLComponents()
-            components.scheme = "https"
-            components.host = "accounts.google.com"
-            components.path = "/o/oauth2/device/code"
-            
-            let params = ["client_id" : "1023522800191-ooltcd9al83vdljjphnmt6cic03fasfk.apps.googleusercontent.com",
-                          "scope" : "email profile"
-            ]
-            
-            components.queryItems = params.map({name, val in
-                URLQueryItem(name: name, value: val)
-            })
-            
-            var request = URLRequest(url: components.url!)
-            request.httpMethod = "POST"
-            
-            URLSession.shared.dataTask(with: request).asDictionary().then { dictionary -> Void in
-                print("Response:\(dictionary)")
-                guard let userCode = dictionary["user_code"] as? String,
-                      let deviceCode = dictionary["device_code"] as? String,
-                      let interval = dictionary["interval"] as? Int else {
-                    let error = NSError(domain: "Login", code: 0, userInfo: [NSLocalizedDescriptionKey : "Generic login failure"])
-                    reject(error)
-                    return 
-                }
-                fulfill(userCode, deviceCode, interval)
-            }
+
+enum LoginConstants {
+    static let maxRetryCount = 5
+}
+
+enum LoginError : LocalizedError {
+    case accessTokenFormat
+    case retryCountExceeded
+    var errorDescription: String? {
+        switch self {
+        case .accessTokenFormat:  return "unexpected access token response"
+        case .retryCountExceeded: return "Retried more than \(LoginConstants.maxRetryCount) times"
         }
     }
+}
+
+struct GoogleSignInController {
+    func requestPresentationCode() -> Promise<NSDictionary> {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "accounts.google.com"
+        components.path = "/o/oauth2/device/code"
+        
+        let params = ["client_id" : "1023522800191-ooltcd9al83vdljjphnmt6cic03fasfk.apps.googleusercontent.com",
+                      "scope" : "email profile"
+        ]
+        
+        components.queryItems = params.map({name, val in
+            URLQueryItem(name: name, value: val)
+        })
+        
+        var request = URLRequest(url: components.url!)
+        request.httpMethod = "POST"
+            
+        return URLSession.shared.dataTask(with: request).asDictionary()
+    }
     
-    func requestAccessToken(fromDeviceCode deviceCode:String, atInterval interval:Int) -> Promise<String> {
-        var retryCount = 0
-        return Promise { fulfill, reject in
-            var components = URLComponents()
-            components.scheme = "https"
-            components.host = "www.googleapis.com"
-            components.path = "/oauth2/v4/token"
-            
-            let params = ["client_id" : "1023522800191-ooltcd9al83vdljjphnmt6cic03fasfk.apps.googleusercontent.com",
-                          "client_secret" : "zqnVCzroDFo2jCkzFB7hdRyy",
-                          "code" : deviceCode,
-                          "grant_type" : "http://oauth.net/grant_type/device/1.0"
-            ]
-            components.queryItems = params.map({name, val in
-                URLQueryItem(name: name, value: val)
-            })
-            var request = URLRequest(url: components.url!)
-            request.httpMethod = "POST"
-            
-            URLSession.shared.dataTask(with: request).asDictionary().then { dictionary -> Promise<String> in
-                print("Response:\(dictionary)")
-                guard let accessToken = dictionary["access_token"] as? String else {
-                    throw LoginError.accessTokenFormat
-                }
-                return Promise(value:accessToken)
-            }.recover { error -> Promise<String> in
-                guard (error as! PMKURLError).canRetry && retryCount < 3 else { throw error }
-                retryCount += 1
-                return after(interval: TimeInterval(interval)).then { _ -> Promise<String> in
-                    self.requestAccessToken(fromDeviceCode: deviceCode, atInterval: interval)
-                }
-            }.catch {error in
-                print ("Error: \(error)")
+    func requestAccessToken(fromDeviceCode deviceCode:String, atInterval interval:Int, retryCount:Int = 0) -> Promise<String> {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "www.googleapis.com"
+        components.path = "/oauth2/v4/token"
+        
+        let params = ["client_id" : "1023522800191-ooltcd9al83vdljjphnmt6cic03fasfk.apps.googleusercontent.com",
+                      "client_secret" : "zqnVCzroDFo2jCkzFB7hdRyy",
+                      "code" : deviceCode,
+                      "grant_type" : "http://oauth.net/grant_type/device/1.0"
+        ]
+        components.queryItems = params.map({name, val in
+            URLQueryItem(name: name, value: val)
+        })
+        var request = URLRequest(url: components.url!)
+        request.httpMethod = "POST"
+        
+        print ("Requesting access token, try \(retryCount+1) of \(LoginConstants.maxRetryCount)...")
+        return URLSession.shared.dataTask(with: request).asDictionary().then { dictionary -> Promise<String> in
+            print("Response:\(dictionary)")
+            guard let accessToken = dictionary["access_token"] as? String else {
+                throw LoginError.accessTokenFormat
+            }
+            return Promise(value:accessToken)
+        }.recover { error -> Promise<String> in
+            guard let pmkError = error as? PMKURLError, pmkError.canRetry else {
+                    throw error
+            }
+            guard retryCount < LoginConstants.maxRetryCount else {
+                throw LoginError.retryCountExceeded
+            }
+            return after(interval: TimeInterval(interval)).then { _ -> Promise<String> in
+                self.requestAccessToken(fromDeviceCode: deviceCode, atInterval: interval, retryCount: retryCount+1)
             }
         }
     }
 }
 
-enum LoginError : LocalizedError {
-    case accessTokenFormat
-}
 /*
  // The below was suggested by https://github.com/mxcl/PromiseKit/issues/594 , but needs coaxing
 extension Promise {
